92cdbd30eca551b85653025a82ec767b8cb637e8
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.brC:/Users/Pedro/AppData/Local/Programs/Python/Python38/python.exe>
2023-02-14 22:38:37 -0300

    'deploy'

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,50 @@ import { Sidebar } from "./components/Sidebar";
 
 
 
-
-const post: { id: number; author: { avatarUrl: string; name: string; role: string; }; content: { type: string; content: string; }[]; publishedAt: Date; }[] = [
-  {
-    id: 1,
-    author: {
-      avatarUrl:
-        "https://pbs.twimg.com/profile_images/1529556363269050369/aje3bVKg_400x400.jpg",
-      name: "Pedro",
-      role: "Web",
-    },
-    content: [
-      { type: "paragraph", content: "Fala galeraa 👋" },
-      {
-        type: "paragraph",
-        content:
-          "Acabei de subir mais um projeto no meu portifa. É um projetoque fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+const post: {
+  id: number;
+  author: { avatarUrl: string; name: string; role: string; };
+  content: { type: 'paragraph' | 'link'; content: string; }[]; publishedAt: Date;
+}[] = [
+    {
+      id: 1,
+      author: {
+        avatarUrl:
+          "https://pbs.twimg.com/profile_images/1529556363269050369/aje3bVKg_400x400.jpg",
+        name: "Pedro",
+        role: "Web",
       },
-      { type: "link", content: "jane.design/doctorcare" },
-    ],
-    publishedAt: new Date("2023-01-07 23:48:00"),
-  },
-  {
-    id: 2,
-    author: {
-      avatarUrl:
-        "https://pbs.twimg.com/profile_images/1529556363269050369/aje3bVKg_400x400.jpg",
-      name: "Lucas",
-      role: "Back",
+      content: [
+        { type: "paragraph", content: "Fala galeraa 👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projetoque fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2023-01-07 23:48:00"),
     },
-    content: [
-      { type: "paragraph", content: "Fala galeraa 👋" },
-      {
-        type: "paragraph",
-        content:
-          "Acabei de subir mais um projeto no meu portifa. É um projetoque fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+    {
+      id: 2,
+      author: {
+        avatarUrl:
+          "https://pbs.twimg.com/profile_images/1529556363269050369/aje3bVKg_400x400.jpg",
+        name: "Lucas",
+        role: "Back",
       },
-      { type: "link", content: "jane.design/doctorcare" },
-    ],
-    publishedAt: new Date("2023-01-07 23:48:00"),
-  },
-];
+      content: [
+        { type: "paragraph", content: "Fala galeraa 👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projetoque fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2023-01-07 23:48:00"),
+    },
+  ];
 export function App() {
   return (
     <>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -17,7 +17,6 @@ interface Content {
   content: string
 }
 
-
 interface PostProps {
   author: Author;
   publishedAt: Date;
